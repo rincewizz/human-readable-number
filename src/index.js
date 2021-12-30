71b65ea5f909d54bf236a45a642ddfc9de12371d
yolf <gleoros@gmail.com>
2021-12-30 05:42:31 +0300

    feat: add implement function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+	let ones=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
+		tens=["", "", "twenty","thirty","forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+	let numberArray = number.toString().split("").map(val=> +val);
+	let humanNumber = "";
+
+	for(let i=0, j=numberArray.length; i<numberArray.length; i++, j--){
+
+		if(j>2){
+			humanNumber+=ones[numberArray[i]]+" hundred ";
+		}else if(j==2 && numberArray[i]>1){
+			humanNumber+=(tens[numberArray[i]]+" ");
+		}else if(j==2 && numberArray[i]==1){
+			humanNumber+=ones[numberArray[i]*10+numberArray[i+1]];
+			break;
+		}
+		else{
+			humanNumber+= (!i || numberArray[i]) ? ones[numberArray[i]] : "";	
+		}
+		
+	}
+	return humanNumber.trim();
 }
